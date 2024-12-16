a57ab5ad80bf9d5fdec852f389dbed43213b2cee
Jai Sandhu <studio@jaisand.hu>
2024-12-16 13:00:28 +0000

    updated portfolio link

--- components/Portfolio/Portfolio.js
+++ components/Portfolio/Portfolio.js
@@ -6,18 +6,25 @@ import PortfolioPage from '../PortfolioPage';
 import PortfolioLogin from '../PortfolioLogin';
 import { useRouter } from 'next/router';
 
-export default function Portfolio({ pageStyle, pageVariants, pageTransition, portfolio, video, password, downloadLink }) {
+export default function Portfolio({ pageStyle, pageVariants, pageTransition, portfolio, video, password, downloadLink, portfolioLink }) {
 	const [{ hasLoggedIn }, { setHasLoggedIn }] = useAppStore();
 	const router = useRouter();
-	
-	useEffect(()=> {
+
+	useEffect(() => {
+		//redirect to portfolio link if there is a portfolio link
+		if (portfolioLink) {
+			window.location = portfolioLink;
+		}
+	}, [portfolioLink]);
+
+	useEffect(() => {
 		const cookies = parseCookies();
 		if ("hasLoggedIn" in cookies) {
 			setHasLoggedIn(true);
 		}
 	}, []);
 
-  return(hasLoggedIn ? 
-		<PortfolioPage pageStyle={pageStyle} pageVariants={pageVariants} pageTransition={pageTransition} portfolio={portfolio} video={video} downloadLink={downloadLink} hidePdf={router?.asPath.substring(1).includes('pdf=no')}/> : 
+	return (hasLoggedIn ?
+		<PortfolioPage pageStyle={pageStyle} pageVariants={pageVariants} pageTransition={pageTransition} portfolio={portfolio} video={video} downloadLink={downloadLink} hidePdf={router?.asPath.substring(1).includes('pdf=no')} /> :
 		<PortfolioLogin pageStyle={pageStyle} pageVariants={pageVariants} pageTransition={pageTransition} password={password} />);
 }
--- pages/[[...slug]].js
+++ pages/[[...slug]].js
@@ -7,68 +7,68 @@ const PortfolioComponent = dynamic(() => import("../components/Portfolio"));
 const TestComponent = dynamic(() => import("../components/Test"));
 
 export default function Page({
-  data,
-  pageStyle,
-  pageTransition,
-  pageVariants,
+	data,
+	pageStyle,
+	pageTransition,
+	pageVariants,
 }) {
-  const docType = data?.docType;
+	const docType = data?.docType;
 
-  // 3. Execute the query again but switch to preview mode if it's in preview mode (backend)
-  const { data: previewData } = usePreviewSubscription(data?.query || null, {
-    params: data?.queryParams ?? {},
-    initialData: data?.pageData,
-    enabled: data?.preview,
-  });
+	// 3. Execute the query again but switch to preview mode if it's in preview mode (backend)
+	const { data: previewData } = usePreviewSubscription(data?.query || null, {
+		params: data?.queryParams ?? {},
+		initialData: data?.pageData,
+		enabled: data?.preview,
+	});
 
-  // 4. Return the correct component based on the docType
-  return (
-    <>
-      {docType === "home" && (
-        <Home
-          pageStyle={pageStyle}
-          pageVariants={pageVariants}
-          pageTransition={pageTransition}
-          {...previewData}
-        />
-      )}
-      {docType === "test" && (
-        <TestComponent
-          pageStyle={pageStyle}
-          pageVariants={pageVariants}
-          pageTransition={pageTransition}
-          {...previewData}
-        />
-      )}
-      {docType === "portfolio" && (
-        <PortfolioComponent
-          pageStyle={pageStyle}
-          pageVariants={pageVariants}
-          pageTransition={pageTransition}
-          {...previewData}
-        />
-      )}
-    </>
-  );
+	// 4. Return the correct component based on the docType
+	return (
+		<>
+			{docType === "home" && (
+				<Home
+					pageStyle={pageStyle}
+					pageVariants={pageVariants}
+					pageTransition={pageTransition}
+					{...previewData}
+				/>
+			)}
+			{/* {docType === "test" && (
+				<TestComponent
+					pageStyle={pageStyle}
+					pageVariants={pageVariants}
+					pageTransition={pageTransition}
+					{...previewData}
+				/>
+			)} */}
+			{docType === "portfolio" && (
+				<PortfolioComponent
+					pageStyle={pageStyle}
+					pageVariants={pageVariants}
+					pageTransition={pageTransition}
+					{...previewData}
+				/>
+			)}
+		</>
+	);
 }
 
 export async function getStaticProps({ params, preview = false }) {
-  // 1 A helper function to work out what query we should run based on this slug
-  const { query, queryParams, docType } = getQueryFromSlug({
-    slug: params?.slug,
-  });
+	// 1 A helper function to work out what query we should run based on this slug
+	const { query, queryParams, docType } = getQueryFromSlug({
+		slug: params?.slug,
+	});
 
-  // 2. Get the initial data for this page, using the correct query
-  const pageData = await getClient(preview).fetch(query, queryParams);
+	// 2. Get the initial data for this page, using the correct query
+	const pageData = await getClient(preview).fetch(query, queryParams);
 
-  return {
-    props: {
-      data: { query, queryParams, docType, pageData, preview },
-    },
-    revalidate: 1,
-  };
+	return {
+		props: {
+			data: { query, queryParams, docType, pageData, preview },
+		},
+		revalidate: 1,
+	};
 }
 
 export async function getStaticPaths({ params, preview = false }) {
-  return { paths: [], fallback: "blocking" };
+	return { paths: [], fallback: "blocking" };
 }
